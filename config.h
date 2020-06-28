39fd9a0647f96fd9688b12d7413ba85034cb3b79
Matas Lauzadis <matas.lauzadis@gmail.com>
2020-06-27 21:27:33 -0500

    Upload config file, removed my personal info

---
+++ config.h
@@ -0,0 +1,11 @@
+#define WIFI_SSID "Your_WiFI_Name"
+#define WIFI_PASSWORD "Your_WiFi_Password"
+
+#define TIMEZONE -5 // Define your timezone to have an accurate clock (hours with respect to GMT +0)
+// "PST": -7 
+// "MST": -6 
+// "CST": -5 
+// "EST": -4 
+// "GMT": 0 
+
+#define ALARM_LENGTH_MINUTES 5 // How long should the alarm run for? [start_time, start_time + ALARM_LENGTH_MINUTES]
